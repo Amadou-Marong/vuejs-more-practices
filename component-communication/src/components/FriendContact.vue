345bac50dffcda2aa52cbdea3c7f547aca649a2c
Amadou-Marong <marongamadou6@gmail.com>
2024-02-27 16:16:32 +0000

    practice on component communication

---
+++ component-communication/src/components/FriendContact.vue
@@ -0,0 +1,94 @@
+<template>
+  <li>
+    <!-- <h2>{{ name }} {{ friendIsFavourite === '1' ? '(Favourite)' : ''}}</h2> -->
+    <!-- <h2>{{ name }} {{ friendIsFavourite ? '(Favourite)' : ''}}</h2> -->
+    <h2>{{ name }} {{ isFavourite ? '(Favourite)' : ''}}</h2>
+    <h2></h2>
+    <button @click="toggleFavourite">Toggle Favourite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+    <button @click="$emit('delete', id)">Delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ['name', 'phoneNumber', 'emailAddress','isFavourite'],
+
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function(value) {
+      //   return value === '1' || value === '0';
+      // }
+    },
+  },
+
+  emits: ['toggle-favourite', 'delete'],
+  // emits: {
+  //   'toggle-favourite': function(id) {
+  //     if(id) {
+  //       return true;
+  //     }else{
+  //       console.warn('Id is Missing')
+  //       return false
+  //     }
+  //   }
+  // },
+  
+  data() {
+    return {
+      detailsAreVisible: false,
+      // friend: {
+      //   id: "amadou",
+      //   name: "Amadou Marong",
+      //   phone: "0123 45678 90",
+      //   email: "amadou@localhost.com",
+      // },
+      // friendIsFavourite: this.isFavourite
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavourite(){
+      // if(this.friendIsFavourite === '1'){
+      //   this.friendIsFavourite = '0'
+      // }else {
+      //   this.friendIsFavourite = '1'
+      // }
+      // this.friendIsFavourite = !this.friendIsFavourite
+      this.$emit('toggle-favourite', this.id);
+    },
+    
+  }
+};
+</script>
