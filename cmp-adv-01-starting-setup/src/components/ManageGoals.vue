ad8e46ea13340b452c17d24d0835090d14438ab6
Amadou-Marong <marongamadou6@gmail.com>
2024-03-05 13:48:39 +0000

    added ref value input

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,4 +1,17 @@
 <template>
     <h2>Manage Goals</h2>
-    <input type="text">
+    <input type="text" ref="goal">
+    <button @click="setGoal">Set Goal</button>
 </template>
+<script>
+    export default {
+        methods: {
+            setGoal() {
+                const enteredValue = this.$refs.goal.value;
+                if (enteredValue.trim() === '') {
+                    alert('Input must not be empty')
+                }
+            }
+        },
+    }
+</script>
