cf266d89b6799b3460c0121be5f9ed8a8649adb6
Amadou-Marong <marongamadou6@gmail.com>
2024-03-05 15:00:30 +0000

    added error alert

---
+++ cmp-adv-01-starting-setup/src/components/ErrorAlert.vue
@@ -0,0 +1,18 @@
+<template>
+    <dialog open>
+        <slot></slot>
+    </dialog>
+</template>
+<style scoped>
+    dialog {
+        position: fixed;
+        top: 20%;
+        left: 25%;
+        width: 40%;
+        border: 1px solid #ccc;
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+        margin: 10px;
+    }
+</style>
--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -2,16 +2,29 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal">
     <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="invalidInput">
+        <p>Input must not be empty</p>
+        <button @click="invalidInput = false">OK</button>
+    </error-alert>
 </template>
 <script>
+    import ErrorAlert from './ErrorAlert.vue';
     export default {
+        components: {
+            ErrorAlert
+        },
+        data() {
+            return {
+                invalidInput: false
+            }
+        },
         methods: {
             setGoal() {
                 const enteredValue = this.$refs.goal.value;
                 if (enteredValue.trim() === '') {
-                    alert('Input must not be empty')
-                }
-            }
+                    this.invalidInput = true;
+                }  
+            },
         },
     }
 </script>
