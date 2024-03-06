dd60ccca1c2dabc4c5cd68225327df95d96acc8f
Amadou-Marong <marongamadou6@gmail.com>
2024-03-06 13:28:22 +0000

    added base card component

--- prj-cmp-01-starting-setup/src/main.js
+++ prj-cmp-01-starting-setup/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import BaseCard from './Components/UI/BaseCard.vue';
 
 const app = createApp(App);
+app.component('base-card', BaseCard);
 app.mount('#app');
