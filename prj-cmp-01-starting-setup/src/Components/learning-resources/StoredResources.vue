d02e2cfa09419305d4a5bfab7c0ab4673ee2847f
Amadou-Marong <marongamadou6@gmail.com>
2024-03-06 11:57:50 +0000

    modified stored resources

--- prj-cmp-01-starting-setup/src/Components/learning-resources/StoredResources.vue
+++ prj-cmp-01-starting-setup/src/Components/learning-resources/StoredResources.vue
@@ -1,19 +1,23 @@
 <template>
-    <li v-for="res in resourses" :key="res.id">
-        <div>
-            <header>
-                <h3>{{ res.title }}</h3>
-                <button>Delete</button>
-            </header>
-        </div>
-        <p>{{ res.description }}</p>
-        <nav>
-            <a :href="res.link">View Resource</a>
-        </nav>
-    </li>
+    <ul>
+        <learning-resource 
+            v-for="res in resourses" 
+            :key="res.id" 
+            :title="res.title" 
+            :description="res.description">
+        </learning-resource>
+    </ul>
 </template>
 <script>
+import LearningResource from './LearningResource.vue';
     export default {
+        components: {
+            LearningResource
+        },
+
         props: ['resourses']
     }
 </script>
+<style scoped>
+    
+</style>
