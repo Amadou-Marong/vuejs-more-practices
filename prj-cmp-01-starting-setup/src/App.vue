b665becdc34bb72e6af09d0148a9f02284c2ea7e
Amadou-Marong <marongamadou6@gmail.com>
2024-03-06 05:22:20 +0000

    first components and props

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul>
+        <li v-for="resource in storedResourses" :key="resource.id">
+            <h2>{{ resource.title }}</h2>
+            <p>{{ resource.description }}</p>
+            <a :href="resource.link">Link</a>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            storedResourses: [
+                {
+                    id: 'official-guide',
+                    title: 'The Official Guide',
+                    description: 'The Official Vuejs Documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'The Official Google Homepage',
+                    link: 'https://google.com'
+                }
+            ]
+        }
+    }
+}
+</script>
