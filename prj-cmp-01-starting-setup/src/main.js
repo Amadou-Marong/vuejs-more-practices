21231f023c076eb5cf96da2f60c9f15a19c68e75
Amadou-Marong <marongamadou6@gmail.com>
2024-03-07 02:06:32 +0000

    model dialog working

--- prj-cmp-01-starting-setup/src/main.js
+++ prj-cmp-01-starting-setup/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import BaseCard from './Components/UI/BaseCard.vue';
 import BaseButton from './Components/UI/BaseButton.vue';
-import Base Dialog from './Components/UI/BaseDialog.vue'
+import BaseDialog from './Components/UI/BaseDialog.vue'
 
 const app = createApp(App);
 app.component('base-card', BaseCard);
