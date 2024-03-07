7eee025c1a36bb5a11d1319c1f6af22b76e02535
Amadou-Marong <marongamadou6@gmail.com>
2024-03-07 02:41:01 +0000

    added delete resource functionality

--- prj-cmp-01-starting-setup/src/Components/learning-resources/TheResources.vue
+++ prj-cmp-01-starting-setup/src/Components/learning-resources/TheResources.vue
@@ -43,7 +43,8 @@ export default {
     provide() {
         return {
             resources: this.storedResources,
-            addResource: this.addResource
+            addResource: this.addResource,
+            deleteResource: this.deleteResource
         }
     },
     computed: {
@@ -52,7 +53,8 @@ export default {
         },
         addResButtonMode() {
             return this.selectedTab === 'add-resource' ? null : 'flat';
-        }
+        },
+        
     },
         
     methods: {
@@ -68,6 +70,10 @@ export default {
             };
             this.storedResources.unshift(newResource);
             this.selectedTab = 'stored-resources'
+        },
+        deleteResource(resId) {
+            const resIndex = this.storedResources.findIndex(res => res.id === resId);
+            this.storedResources.splice(resIndex, 1)
         }
     }
 }
